426badf606f6822a48a3a9e1dbaf8a0881cfc009
Ethan <ethanjwrightson@gmail.com>
2018-08-25 12:53:43 +0100

    Set up the Table View, with some test code in the cells.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Ethan Wrightson on 25/08/2018.
+//  Copyright © 2018 Ethan Wrightson. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mawi", "Cross Ocean", "Restore Heart"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    ///////////////////////////////////////////////////////////////////
+    //MARK: - TableView methods
+    
+    // Datasource methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print("Creating cell.")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    // Delegate methods
+
+}
+
