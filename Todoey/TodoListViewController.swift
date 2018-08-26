bc5d5eba7cff66c6ff5f85256afc9c2dbf7d0069
Ethan <ethanjwrightson@gmail.com>
2018-08-26 14:14:01 +0100

    Add in functionality for 'add item'. Still need to add in functionality for 'delete item'.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mawi", "Cross Ocean", "Restore Heart"]
+    var itemArray = ["Find Mawi", "Cross Ocean", "Restore Heart"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,7 +48,7 @@ class TodoListViewController: UITableViewController {
     
     
     
-    // Delegate methods
+    //MARK: Delegate methods
     
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -64,6 +64,60 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
+    
+    
+    
+    ///////////////////////////////////////////////////
+    //MARK: Functionality
+    
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        var textField : UITextField = UITextField() // This will be the text field inside the pop-up for new items.
+        
+        
+        // Add a pop-up option that will allow the user to add a new item to the to-do list
+        
+        let alert = UIAlertController(title: "Add new item:", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add item", style: .default) {
+            (action) in
+            
+            // Completion handler for the action. Will run when the user has clicked 'Add item' in the alert
+            
+            if let textEntered = textField.text {
+                
+                print("User has clicked \'Add Item\'. Inside completion handler")
+                print("The item is called: \(textEntered)")
+                
+                self.itemArray.append(textEntered) // Add the item to the array of items
+                
+                // Update the table view
+                self.tableView.reloadData()
+                
+            }
+            
+        }
+        
+        alert.addAction(action)
+        
+        
+        
+        
+        // Add the text field where the user can enter the title of the item
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+        }
+        
+        
+        
+        present(alert, animated: true, completion: nil) // Show the alert
+        
+    }
+    
 
 }
 
